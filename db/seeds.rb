581668c2ad3015782c2dceb43a333fe2646d25da
seanmcc1982 <seanpaulmccabe@gmail.com>
2017-10-20 23:07:43 +0000

    bookstore mvc

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Advert.create(name:"iPad",description:"selling the IPad",price:100.0,seller_id:1,email:"seanmcc@space.org")
+Advert.create(name:"iPad2",description:"selling the IPad2",price:100.0,seller_id:2,email:"seanmcc2@space.org")
